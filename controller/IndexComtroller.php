1fd072507c272f09158190331685ba829b3f2d8c
甄健壮 <zhenjianzhuang@3ncto.com>
2016-02-18 16:54:27 +0800

    新建测试分支并提交

---
+++ controller/IndexComtroller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* add SeanZen
+* date 2016-02-18
+*/
+class IndexControllerClass extends AnotherClass
+{
+	
+	function __STRUCE(){
+		
+	}
+
+	public function indexAction(argument)
+	{
+		# code...
+	}
+}
